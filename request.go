e9907b94952cdcb458e98b49c7682ba814a78413
John Wang <johncwang@gmail.com>
2023-11-26 02:57:04 -0800

    style: `NewHTTPRequest()`: update scope and error message

--- request.go
+++ request.go
@@ -2,7 +2,7 @@ package awslambda
 
 import (
 	"context"
-	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 
@@ -12,18 +12,25 @@ import (
 // NewHTTPRequest returns an `*http.Request` given an `events.APIGatewayProxyRequest`.
 func NewHTTPRequest(ctx context.Context, req events.APIGatewayProxyRequest) (*http.Request, error) {
 	// Adaptation from https://stackoverflow.com/a/71727175/1908967
-	httpReq, err := http.NewRequestWithContext(
+	if httpReq, err := http.NewRequestWithContext(
 		ctx,
 		req.HTTPMethod,
 		req.Path,
-		strings.NewReader(req.Body))
-	if err != nil {
-		return nil, errors.New("failed to convert a lambda req into a http req")
+		strings.NewReader(req.Body)); err != nil {
+		return nil, wrapError("failed to convert lambda request to http request", err)
+	} else {
+		// some headers may be important, let get all of them, just in case
+		for name, value := range req.Headers {
+			httpReq.Header.Add(name, value)
+		}
+		return httpReq, nil
 	}
+}
 
-	// some headers may be important, let get all of them, just in case
-	for name, value := range req.Headers {
-		httpReq.Header.Add(name, value)
+func wrapError(msg string, err error) error {
+	if err == nil || msg == "" {
+		return err
+	} else {
+		return fmt.Errorf("%s: [%w]", msg, err)
 	}
-	return httpReq, nil
 }
